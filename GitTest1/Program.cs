673cd4e8e4b456c5e986bb6e3b9d82b01eae8814
John Garrett <john_g_garrett@hotmail.co.uk>
2016-09-02 11:39:08 +0100

    New file

---
+++ GitTest1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest1
+{
+    class Program
+    {
+        //tbd
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
